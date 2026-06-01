3d9a44bcc4d5b6a3487689592b1a2ae532563c54
ECHSFXY <88482848+echs-top@users.noreply.github.com>
2026-06-02 05:42:37 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -7,23 +7,23 @@ export default {
       "Access-Control-Allow-Headers": "*",
     };
 
-    // OPTIONS预检请求
+    // 预检
     if (request.method === "OPTIONS") {
       return new Response(null, { headers: corsHeaders });
     }
 
     const url = new URL(request.url);
 
-    // 1. 优先处理根路径重定向
+    // 重定向
     if (url.pathname === "/") {
       return Response.redirect("https://github.com/echs-top/proxy", 302);
     }
 
     try {
-      // 2. 从全局绑定的静态资源（即 dist 目录）中获取文件
+      // 获取文件
       const response = await env.ASSETS.fetch(request);
 
-      // 文件不存在返回404
+      // 404
       if (response.status === 404) {
         return new Response("File not found", { status: 404 });
       }
